f05b2507bf15edde9506212942923b9afd491aff
Joel Mathew <joelmathew.mec@gmail.com>
2015-12-19 17:32:13 -0600

    Found all answers.

--- dp.py
+++ dp.py
@@ -15,28 +15,28 @@ def subset_sum(n, W):
                 M[i][w] = max(M[i-1][w], wi + M[i-1][w-wi])
     return M
 
-def find_sol(i, w):
-    wi = len(words[i-1])
-    if i <= 0:
-        return
-    if w - wi < 0:
-        wfile.write(words[i-1] + ', ')
-        find_sol(i-1, w)
-    elif M[i][w] == M[i-1][w] and M[i][w] == (wi + M[i-1][w-wi]):
-        find_sol(i-1, w)
-    elif M[i][w] == (wi + M[i-1][w-wi]):
-        wfile.write(words[i-1] + ', ')
-        find_sol(i-1, w-wi)
+#def find_sol(i, w):
+#    wi = len(words[i-1])
+#    if i <= 0:
+#        return
+#    if w - wi < 0:
+#        wfile.write(words[i-1] + ', ')
+#        find_sol(i-1, w)
+#    elif M[i][w] == M[i-1][w] and M[i][w] == (wi + M[i-1][w-wi]):
+#        find_sol(i-1, w)
+#    elif M[i][w] == (wi + M[i-1][w-wi]):
+#        wfile.write(words[i-1] + ', ')
+#        find_sol(i-1, w-wi)
 
 def build_tree():
     stack = []
     tree = {}
     
-    tree['root'] = []
+    tree['__root'] = []
     for child in find_children((nval+1,Wval+1)):
         stack.append(child)
-        tree['root'].append(words[child[0]-1])
-     while True:
+        tree['__root'].append(words[child[0]-1])
+    while True:
         counter = 0
         while counter <= Wval:
             if len(stack) < 1:
@@ -69,19 +69,36 @@ def find_children(base):
         i -= 1
     children.append((i,w))
     return children
-                                                    
+
+def traverse(n, counter, route):
+    if counter == max_val:
+#        print route
+        wfile.write(','.join(route)+'\n')
+    if counter > max_val:
+        return
+    children = graph[n]
+    if children:
+        for child in children:
+            temp_route = route[:]
+            temp_route.append(child)
+            traverse(child, counter+len(child), temp_route)
+
 words = ['yo', 'no', 'him', 'tommy', 'so', 'a']
 nval = 6
 Wval = 6
 M = subset_sum(nval+1, Wval+1)
 print M
-print 'Done processing. The max value is', M[nval][Wval]
-if M[nval][Wval] <= 0:
+max_val = M[nval][Wval]
+print 'Done processing. The max value is', max_val
+if max_val <= 0:
     print 'No solution found.'
     exit()
 
 wfile = open('output.txt', 'w')
-find_sol(nval, Wval)
 #print find_children((7,7))
-print build_tree()
+#print build_tree()
+graph = build_tree()
+print graph
+for root in graph['__root']:
+    traverse(root,len(root),[root])
 wfile.close()
